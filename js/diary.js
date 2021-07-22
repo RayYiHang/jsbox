65a07259975ef5229c6587f5857882f34588102d
Arcadia <RayYiHang@users.noreply.github.com>
2021-07-22 19:18:42 +0800

    change diary

--- js/diary.js
+++ js/diary.js
@@ -1,4 +1,4 @@
-var obj = JSON.parse($response.body);
-obj.expires_at = "2029-07-27T03:03:58.000000";
-obj.membership_status = premium;
+let obj = JSON.parse($response.body);
+obj.data.expires_at = "2029-07-27T03:03:58.000000";
+obj.data.membership_status = premium;
 $done({body:JSON.stringify(obj)});
