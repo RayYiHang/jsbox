14d46363b86ac73de8d737cd40e75b2d26096aca
Arcadia <RayYiHang@users.noreply.github.com>
2021-07-25 12:43:02 +0800

    change zhihu2.js

--- js/zhihu2.js
+++ js/zhihu2.js
@@ -1,9 +1,4 @@
 let obj = JSON.parse($response.body);
-let resultData = [];
-obj["data"].forEach(function(obj) {
-  if ("offset" in obj) {
-    resultData.push(obj)
-  }
-});
+let resultData = obj["data"].filter((item) => "offset" in item);
 obj["data"] = resultData;
 $done({body:JSON.stringify(obj)});
