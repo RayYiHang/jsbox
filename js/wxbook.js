4ea31e40cf05425b62bec82d6b1d2a5a16c3bc21
Arcadia <RayYiHang@users.noreply.github.com>
2021-07-27 20:06:43 +0800

    change wxbook.js

--- js/wxbook.js
+++ js/wxbook.js
@@ -1,6 +1,6 @@
 let body = JSON.parse($response.body);
 delete body["hintsForRecharge"];
-body["isPaying"] = 2;
+body["isPaying"] = 1;
 body["payingRemainTime"] = 9627401599;
 body["remainTime"] = 9627401599;
 body["expiredTime"] = 9627401599;
