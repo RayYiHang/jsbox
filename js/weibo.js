48a9a19155d005940ccdc67d5300dc09a0b5b651
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2021-07-30 13:13:53 +0800

    Update weibo.js

--- js/weibo.js
+++ js/weibo.js
@@ -1,3 +1,7 @@
 let obj = JSON.parse($response.body);
-delete obj["ad"];
+let resultData = obj["statuses"].filter((item) => {
+  return item["readtimetype"] == "mblog"
+})
+obj["statuses"] = resultData;
 $done({body:JSON.stringify(obj)});
+
