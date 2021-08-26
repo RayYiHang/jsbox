43ebd4b6fb4a438e3c4adff48b822e7b1324953e
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2021-08-26 22:08:04 +0800

    Update Cuto.js

--- js/Cuto.js
+++ js/Cuto.js
@@ -1,5 +1,5 @@
-let obj = JSON.parse($response.body);
-let subscriber = JSON.parse("""{
+var obj = JSON.parse($response.body);
+var subscriber = {
 "is_sandbox" : false,
 "ownership_type" : "PURCHASED",
 "billing_issues_detected_at" : null,
@@ -10,13 +10,13 @@ let subscriber = JSON.parse("""{
 "original_purchase_date" : "2021-07-15T14:59:36Z",
 "purchase_date" : "2021-07-15T14:59:35Z",
 "store" : "app_store"
-}""");
-let entitlement = JSON.parse(""""pro" : {
+};
+var entitlement = {
 "grace_period_expires_date" : null,
 "purchase_date" : "2021-07-15T14:59:35Z",
 "product_identifier" : "com.potatsolab.cuto.pro",
 "expires_date" : "2029-08-11T12:33:17Z"
-}""");
+};
 obj["subscriber"]["subscriptions"]["com.potatsolab.cuto.pro"] = subscriber;
 obj["subscriber"]["entitlements"]["pro"] = entitlement;
 $done({body:JSON.stringify(obj)});
