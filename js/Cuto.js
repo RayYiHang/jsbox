f9f79ddad3bc480c5d9ea84dff7cec221bcefc60
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2021-08-27 08:41:21 +0800

    Update Cuto.js

--- js/Cuto.js
+++ js/Cuto.js
@@ -4,6 +4,7 @@ var subscriber = {
 "ownership_type" : "PURCHASED",
 "billing_issues_detected_at" : null,
 "period_type" : "purchased",
+"expires_date" : "2029-08-11T12:33:17Z",
 "grace_period_expires_date" : null,
 "original_purchase_date" : "2016-04-10T05:28:18Z",
 "purchase_date" : "2021-07-15T14:59:35Z",
@@ -12,7 +13,8 @@ var subscriber = {
 var entitlement = {
 "grace_period_expires_date" : null,
 "purchase_date" : "2021-07-15T14:59:35Z",
-"product_identifier" : "com.potatsolab.cuto.pro"
+"product_identifier" : "com.potatsolab.cuto.pro",
+"expires_date" : "2029-08-11T12:33:17Z"
 };
 obj["subscriber"]["subscriptions"]["com.potatsolab.cuto.pro"] = subscriber;
 obj["subscriber"]["entitlements"]["pro"] = entitlement;
