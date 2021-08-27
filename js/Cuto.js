cea45a58c349808db982f09c3158065288209537
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2021-08-27 08:37:34 +0800

    Update Cuto.js

--- js/Cuto.js
+++ js/Cuto.js
@@ -1,9 +1,9 @@
 var obj = JSON.parse($response.body);
 var subscriber = {
 "is_sandbox" : false,
-"ownership_type" : "PURCHASE",
+"ownership_type" : "PURCHASED",
 "billing_issues_detected_at" : null,
-"period_type" : "purchase",
+"period_type" : "purchased",
 "grace_period_expires_date" : null,
 "original_purchase_date" : "2016-04-10T05:28:18Z",
 "purchase_date" : "2021-07-15T14:59:35Z",
