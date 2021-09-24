69d087f7bbb241a90576a3fdd059c5f03ce49bf8
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2021-09-24 19:20:56 +0800

    Create 1blocker.js

---
+++ js/1blocker.js
@@ -0,0 +1,17 @@
+var obj = JSON.parse($response.body);
+var subscriber = [{
+"id" : "4ec05ff042",
+"is_sandbox" : false,
+"purchase_date" : "2021-07-15T14:59:35Z",
+"original_purchase_date" : "2021-07-15T14:59:35Z",
+"store" : "app_store"
+}];
+var entitlement = {
+"grace_period_expires_date" : null,
+"purchase_date" : "2021-07-15T14:59:35Z",
+"product_identifier" : "blocker.ios.iap.lifetime",
+"expires_date" : null
+};
+obj["subscriber"]["non_subscriptions"]["blocker.ios.iap.lifetime"] = subscriber;
+obj["subscriber"]["entitlements"]["pro"] = entitlement;
+$done({body:JSON.stringify(obj)});
