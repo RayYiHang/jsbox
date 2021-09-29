db064ccfdfec00de6ce4c600bf0941b8ae663222
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2021-09-29 20:59:50 +0800

    Update grow.js

--- js/grow.js
+++ js/grow.js
@@ -1,17 +1,26 @@
 var obj = JSON.parse($response.body);
 var subscriber = [{
-"id" : "4ec05ff042",
-"is_sandbox" : false,
-"purchase_date" : "2021-07-15T14:59:35Z",
-"original_purchase_date" : "2021-07-15T14:59:35Z",
-"store" : "app_store"
+"grow_lifetime":{
+         "is_sandbox":false,
+         "ownership_type":"PURCHASED",
+         "billing_issues_detected_at":null,
+         "period_type":"purchase",
+         "expires_date":"2029-08-11T12:33:17Z",
+         "grace_period_expires_date":null,
+         "unsubscribe_detected_at":"2021-07-15T15:08:38Z",
+         "original_purchase_date":"2021-07-15T14:59:36Z",
+         "purchase_date":"2021-07-15T14:59:35Z",
+         "store":"app_store"
+       }
 }];
 var entitlement = {
-"grace_period_expires_date" : null,
-"purchase_date" : "2021-07-15T14:59:35Z",
-"product_identifier" : "grow_lifetime",
-"expires_date" : null
+"pro":{
+         "grace_period_expires_date":null,
+         "purchase_date":"2021-07-15T14:59:35Z",
+         "product_identifier":"grow_lifetime",
+         "expires_date":"2029-08-11T12:33:17Z"
+       }
 };
-obj["subscriber"]["non_subscriptions"]["grow_lifetime"] = subscriber;
-obj["subscriber"]["entitlements"]["premium"] = entitlement;
+obj["subscriber"]["subscriptions"]["grow_lifetime"] = subscriber;
+obj["subscriber"]["entitlements"]["pro"] = entitlement;
 $done({body:JSON.stringify(obj)});
