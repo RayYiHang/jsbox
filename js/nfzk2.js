66be3a43974cca6080402972c1811d0cdb53e794
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2021-11-02 21:38:15 +0800

    Create nfzk2.js

---
+++ js/nfzk2.js
@@ -0,0 +1,11 @@
+let obj = JSON.parse($response.body);
+obj["data"]["user"] = {
+  "isview" : true,
+        "isdigg" : true,
+        "isNewsStand" : 2,
+        "islogin" : true,
+        "expire_time" : "2030-05-05",
+        "isfav" : true,
+        "member_type" : 5
+};
+$done({body:JSON.stringify(obj)});
