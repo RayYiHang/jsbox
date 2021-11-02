c8472d38927baf141c046d9adad81af96316f1c7
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2021-11-02 20:38:47 +0800

    Create quiz.js

---
+++ js/quiz.js
@@ -0,0 +1,4 @@
+let obj = JSON.parse($response.body);
+obj["responses"][0]["models"]["user"][0]["type"] = 1;
+obj["responses"][0]["models"]["user"][0]["_isEligibleForFreeTrial"] = false;
+$done({body:JSON.stringify(obj)});
