bfb8a0473c3cc5986d257c5eb9960263a4050313
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2021-11-02 20:53:53 +0800

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,4 +1,4 @@
 let obj = JSON.parse($response.body);
 obj["responses"][0]["models"]["user"][0]["type"] = 1;
-obj["responses"][0]["models"]["user"][0]["_isEligibleForFreeTrial"] = false;
+//obj["responses"][0]["models"]["user"][0]["_isEligibleForFreeTrial"] = false;
 $done({body:JSON.stringify(obj)});
