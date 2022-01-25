f14e8966cb6ac0ec4f01096828d55276b7f3f0c8
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2022-01-25 21:32:34 +0800

    Update facer.js

--- js/facer.js
+++ js/facer.js
@@ -1,4 +1,4 @@
 var modifiedStatus = 'HTTP/1.1 200 OK';
 let obj;
-obj = ["result"]["isActive"] = true;
+obj = {"result":{"isActive":true,"platform":"apple"}}
 $done({body:JSON.stringify(obj), status: modifiedStatus});
