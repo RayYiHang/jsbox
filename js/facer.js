16a64d2c92c051a99f2002ea128c87d85228d0a4
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2022-01-25 21:22:16 +0800

    Update facer.js

--- js/facer.js
+++ js/facer.js
@@ -1,4 +1,4 @@
-$response.statusCode = "200";
+var modifiedStatus = 'HTTP/1.1 200 OK';
 let obj = JSON.parse($response.body);
 obj["result"]["isActive"] = true;
-$done({body:JSON.stringify(obj)});
+$done({body:JSON.stringify(obj), status: modifiedStatus});
