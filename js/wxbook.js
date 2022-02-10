4ab00d0034db1d15cb29d040d6aed43ec2d4f04a
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2022-02-10 20:09:10 +0800

    Update wxbook.js

--- js/wxbook.js
+++ js/wxbook.js
@@ -4,4 +4,6 @@ body["isPaying"] = 1;
 body["payingRemainTime"] = 9627401599;
 body["remainTime"] = 9627401599;
 body["expiredTime"] = 9627401599;
+body["permanent"] = 1;
+body["totalFreeReadDay"] = 9999;
 $done({body:JSON.stringify(body)});
