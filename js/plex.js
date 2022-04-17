8e20d2d71706da58af31074479433ea3997b5e8d
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2022-04-17 10:55:43 +0800

    plex.js

---
+++ js/plex.js
@@ -0,0 +1,34 @@
+let obj = {
+  "data" : {
+    "is_oauth" : 0,
+    "need_set_password" : false,
+    "unit_of_expire_in" : "D",
+    "nickname" : "rayemail1234@gmail.com",
+    "calling_code" : "",
+    "award_days" : 0,
+    "value_of_expire_in" : 0,
+    "invitation_code" : "WTXGNU",
+    "changed" : false,
+    "gfwlist_url" : "https://pkg.aiaster.xyz/plex/configs/gfwlist.txt",
+    "should_display_advertisement" : 1,
+    "expire_at" : 1850162293000,
+    "unread_message" : 0,
+    "expire_remind_type" : "NEVER",
+    "email_verified" : false,
+    "id" : 1910747,
+    "phone_num" : "",
+    "device_disconnect_minutes" : 4,
+    "email" : "rayemail1234@gmail.com",
+    "has_password" : true,
+    "itunes_subscription_cancelled" : false,
+    "ad_time" : 5000,
+    "max_device_count" : 1,
+    "avatar" : "",
+    "role" : "trier",
+    "itunes_subscribed" : true,
+    "access_token" : "l1bLnVDoys1cQKeBWBFq1mW2AY02xoED",
+    "beyond_device_limit" : false
+  },
+  "code" : 0
+};
+$done({body: JSON.stringify(obj)});
