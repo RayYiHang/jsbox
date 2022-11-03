e9b5c6e4fd85431afd9603633f58b28e934534dd
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2022-11-03 12:19:37 +0800

    Update planny.js

--- js/planny.js
+++ js/planny.js
@@ -14,9 +14,9 @@ var subscriber = [{
 var entitlement = {
 "grace_period_expires_date" : null,
 "purchase_date" : "2021-07-15T14:59:35Z",
-"product_identifier" : "com.kevinreutter.Sagittarius.PremiumLifetime",
+"product_identifier" : "com.kevinreutter.Sagittarius.Premium1YearOffer",
 "expires_date": "2029-08-11T12:33:17Z"
 };
-obj["subscriber"]["subscriptions"]["com.kevinreutter.Sagittarius.PremiumLifetime"] = subscriber;
+obj["subscriber"]["subscriptions"]["com.kevinreutter.Sagittarius.Premium1YearOffer"] = subscriber;
 obj["subscriber"]["entitlements"]["pro"] = entitlement;
 $done({body:JSON.stringify(obj)});
