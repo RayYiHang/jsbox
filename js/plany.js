f951544fcb262f93b56a00096378c17e4099d3f4
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2022-11-03 10:50:59 +0800

    Update plany.js

--- js/plany.js
+++ js/plany.js
@@ -1,11 +1,16 @@
 var obj = JSON.parse($response.body);
 var subscriber = [{
-"id" : "4ec05ff042",
-"is_sandbox" : false,
-"purchase_date" : "2021-07-15T14:59:35Z",
-"original_purchase_date" : "2021-07-15T14:59:35Z",
-"store" : "app_store"
-}];
+         "is_sandbox":false,
+         "ownership_type":"PURCHASED",
+         "billing_issues_detected_at":null,
+         "period_type":"purchase",
+         "expires_date":"2029-08-11T12:33:17Z",
+         "grace_period_expires_date":null,
+         "unsubscribe_detected_at":"2021-07-15T15:08:38Z",
+         "original_purchase_date":"2021-07-15T14:59:36Z",
+         "purchase_date":"2021-07-15T14:59:35Z",
+         "store":"app_store"
+       }];
 var entitlement = {
 "grace_period_expires_date" : null,
 "purchase_date" : "2021-07-15T14:59:35Z",
