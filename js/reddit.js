643268d942b11414cfa25a8ecf46ba4e59075e6e
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-02-12 10:19:52 +0800

    Update reddit.js

--- js/reddit.js
+++ js/reddit.js
@@ -1,5 +1,7 @@
-let res = JSON.parse($response.body);
-let resultData = res["data"]["home"]["elements"]["edges"].filter((item) => !item["node"]["isCreatedFromAdsUi"]);
-res["data"]["home"]["elements"]["edges"] = resultData;
+let obj = JSON.parse($response.body);
+for( var i = 0; i < obj.length-1; i++){ 
+   if ( obj["data"]["home"]["elements"]["edges"][i]["node"]["isCreatedFromAdsUi"] === true) {
+     obj["data"]["home"]["elements"]["edges"].splice(i, 1); 
+   }
 }
-$done({body:JSON.stringify(res)});
+$done({body:JSON.stringify(obj)});
