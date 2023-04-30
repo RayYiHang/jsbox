dcfaf0565a5bf88c03b1abf4eaf8d6eb7fe0145c
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-04-30 23:44:57 +1000

    Create wins.js

---
+++ js/wins.js
@@ -0,0 +1,13 @@
+var body = $response.body.replace(/.*/, '{
+  "success": true,
+  "response": {
+    "invoice_id": 1,
+    "subscription_id": 1,
+    "amount": "10.00",
+    "currency": "USD",
+    "payment_date": "2023-04-21",
+    "receipt_url": "https://my.paddle.com/receipt/1-1/3-chre8a53a2724c6-42781cb91a",
+    "status": "success"
+  }
+}')
+$done({ body })
