0c76634e2822081c6f65d8b950b6d60ff22bc25c
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-05-01 00:35:13 +1000

    Update wins.js

--- js/wins.js
+++ js/wins.js
@@ -1,13 +1,11 @@
 const obj = {
-  "success": true,
-  "response": {
-    "invoice_id": 1,
-    "subscription_id": 1,
-    "amount": "10.00",
-    "currency": "USD",
-    "payment_date": "2023-04-21",
-    "receipt_url": "https://my.paddle.com/receipt/1-1/3-chre8a53a2724c6-42781cb91a",
-    "status": "success"
-  }
-};
+    "success": true,
+    "response": {
+        "product_id": "781505",
+        "activation_id": "admin",
+        "type": "personal",
+        "expires": 1,
+        "expiry_date": 1999999999999
+    }
+}
 $done({body: JSON.stringify(obj)});
