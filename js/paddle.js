82f4ad2ad1f1c5f4ee1c0b890631ade805324ef8
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-05-01 12:20:56 +1000

    Update paddle.js

--- js/paddle.js
+++ js/paddle.js
@@ -3,17 +3,12 @@ console.log(typeof requestBody)
 console.log(requestBody)
 
 
-const params = {};
-requestBody.split('&').forEach(pair => {
-  const [key, value] = pair.split('=');
-  params[decodeURIComponent(key)] = decodeURIComponent(value);
-});
-console.log(params.product_id); 
+
 
 const obj = {
     "success": true,
     "response": {
-        "product_id": params.product_id,
+        "product_id": 827382,
         "activation_id": "admin",
         "type": "personal",
         "expires": 1,
