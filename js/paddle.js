b0e0b01577e22d86526f36bcffe108bac547b599
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-05-01 11:31:27 +1000

    Update and rename wins.js to paddle.js

--- js/paddle.js
+++ js/paddle.js
@@ -1,7 +1,13 @@
+let str = $request.body;
+console.log(str);
+const regex = /product_id=(\d{6})/g;
+const matches = str.match(regex);
+
+console.log(matches); // ["123abc", "456def", "789ghi"]
 const obj = {
     "success": true,
     "response": {
-        "product_id": "781505",
+        "product_id": matches[1],
         "activation_id": "admin",
         "type": "personal",
         "expires": 1,
