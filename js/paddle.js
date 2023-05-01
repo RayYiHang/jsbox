76dc81ebc436f27cdde8da63b3023a2a11ee77a2
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-05-01 12:26:40 +1000

    Update paddle.js

--- js/paddle.js
+++ js/paddle.js
@@ -1,14 +1,7 @@
-const requestBody = $request;
-console.log(typeof requestBody)
-console.log(requestBody)
-
-
-
-
 const obj = {
     "success": true,
     "response": {
-        "product_id": 827382,
+        "product_id": 561930,
         "activation_id": "admin",
         "type": "personal",
         "expires": 1,
