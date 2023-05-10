bb909486d940fad3199cc85e5335a1e5f6511892
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-05-10 17:19:56 +1000

    Update and rename macwhisper5.js to macwhisper6.js

---
+++ js/macwhisper6.js
@@ -0,0 +1,11 @@
+var obj = {
+    "status": "valid",
+    "name": "John Doe",
+    "expiration_date": "2029-12-31",
+    "features": [
+        "feature1",
+        "feature2",
+        "feature3"
+    ]
+}
+$done({body:JSON.stringify(obj)});
