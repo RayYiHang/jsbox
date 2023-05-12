45b1012a91a41a36b8de0f5b08d79ea9f358fe0b
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-05-12 14:35:43 +1000

    Update macwhisper9.js

--- js/macwhisper9.js
+++ js/macwhisper9.js
@@ -1,8 +1,10 @@
 var obj = {
     "success": true,
+    "response": {
     "freeTrialProduct": false,
     "subscriptions": true,
     "hasFullAccess": true,
     "activeProduct": true
+    };
 }
 $done({body:JSON.stringify(obj)});
