c9753787c02fb1132c400cc0bbe4f89bafa34cc3
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-05-12 14:33:42 +1000

    Update and rename macwhisper8.js to macwhisper9.js

---
+++ js/macwhisper9.js
@@ -0,0 +1,8 @@
+var obj = {
+    "success": true,
+    "freeTrialProduct": false,
+    "subscriptions": true,
+    "hasFullAccess": true,
+    "activeProduct": true
+}
+$done({body:JSON.stringify(obj)});
